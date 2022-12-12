b4b1bb40cf8d808d06acbece91df1bfbb9e1c7d7
Irfan Azim <irfanteipov@gmail.com>
2022-12-12 22:11:48 +0300

    Updated Person Class

--- src/ScrumTeam/Person.java
+++ src/ScrumTeam/Person.java
@@ -1,4 +1,27 @@
 package ScrumTeam;
 
 public class Person {
+
+    public String name;
+    public int age;
+    public char gender;
+
+    public void setInfo(String name, int age, char gender){
+        this.name = name;
+        this.age= age;
+        this.gender = gender;
+    }
+
+
 }
+/*
+ Create a class named Person
+			variables:
+				name, age, gender
+
+			methods:
+				setInfo(): sets the name, age, gender of person
+				eat(String food)
+				drink(String drink)
+				toString()
+ */
