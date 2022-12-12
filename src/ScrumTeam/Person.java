4d0f0bb7f0aa01ecb0bfb6f29bb76b7982b46cb1
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-12 23:06:59 +0300

    Person created

--- src/ScrumTeam/Person.java
+++ src/ScrumTeam/Person.java
@@ -4,14 +4,9 @@ public class Person {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        String name;
 
-        System.out.println("Group 12");
 
-        // work 1
-        // work 1
-        // work 1
-        // work 1
 
     }
 }
